fd6a2f08d3d619a6269f3a2892b6aa0461cec3ae
Asti-ja-Silver <160499851+Asti-ja-Silver@users.noreply.github.com>
2024-02-19 16:14:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('rsvpButton').addEventListener('click', function() {
+    // Replace 'your-form-url' with the URL of your RSVP form
+    window.location.href = 'https://forms.gle/XbKuGnNYcobVjhEe9';
+});
