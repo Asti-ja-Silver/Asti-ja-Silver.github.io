7883975f37a0d0d4acde2cae6f03216ff357ef54
Kersti <kkuuse@tlu.ee>
2024-02-19 16:54:30 +0200

    style edist

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById('rsvpButton').addEventListener('click', function() {
-    // Replace 'your-form-url' with the URL of your RSVP form
-    window.location.href = 'https://forms.gle/XbKuGnNYcobVjhEe9';
+    window.location.href = 'https://forms.gle/aq9u7r7q79WjRCY47';
 });
