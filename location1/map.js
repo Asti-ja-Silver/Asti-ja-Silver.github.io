32fce907963de6a7c2f7aa9f0cae0fe79f4ee3e5
Kersti <kkuuse@tlu.ee>
2024-08-31 19:12:08 +0300

    leaflet map

---
+++ location1/map.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const map = L.map('map').setView([57.7458889, 27.1047735], 15);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
+  map.attributionControl.remove();
+
+  //custom icon
+  const pointerIcon = L.icon({
+    iconUrl: '../src/pointer.png',
+    iconSize: [40, 40],
+    iconAnchor: [25, 50],  // horizontal offset, vertical offset
+    popupAnchor: [0, -50]   // Point from which the popup should open relative to the iconAnchor
+  });
+
+  L.marker([57.7458889, 27.1047735], { icon: pointerIcon }).addTo(map)
+    .bindPopup('<b>Põlluke</b><br>Kohtume siin!')
+    .openPopup();
+});
