3afaad42f37e2ec7cbcbd0fe124a206fb8c0cefd
Kersti <kkuuse@tlu.ee>
2024-09-13 22:40:02 +0300

    paths changed

--- location1/map.js
+++ location1/map.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  const map = L.map('map').setView([57.73296606485817, 27.0848579323196], 15);
+  const map = L.map('map').setView([57.7330000, 27.0850000], 15);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
     popupAnchor: [0, -25]   // Point from which the popup should open relative to the iconAnchor
   });
 
-  L.marker([57.73296606485817, 27.0848579323196], { icon: pointerIcon }).addTo(map)
+  L.marker([57.7330000, 27.0850000], { icon: pointerIcon }).addTo(map)
     .bindPopup('<b>Põld</b><br>Kohtume siin!')
     .openPopup();
 });
